0dd441d0f8ca7e710f07b45c21ef0a0c91dca7d2
Sylvain Vansteenkiste <sylvain.vansteenkiste59@gmail.com>
2022-07-25 10:13:49 +0200

    Login End

--- src/main/java/fr/example/sec/SecurityConfig.java
+++ src/main/java/fr/example/sec/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
